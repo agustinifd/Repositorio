cbb75fa22e8d36e5db695c120c8af35951d0fa36
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-08-16 16:30:47 -0300

    entrega16del8
    entrega 16del8

--- js/init.js
+++ js/init.js
@@ -1,86 +1,3 @@
-const sinlogear = document.querySelectorAll(".sinlogear")
-const logeado = document.querySelectorAll(".logeado")
-
-const loginCheck = user => {
-    if (user) {
-logeado.forEach(link => link.style.display = "block");
-sinlogear.forEach(link => link.style.display = "none");
-} else {
-    logeado.forEach(link => link.style.display = "none");
-    sinlogear.forEach(link => link.style.display = "block")
-}
-}
-
-
-//// REGISTRO USUARIO /////
-
-const Registroform = document.querySelector("#Registro-form");
-Registroform.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const Registropassword = document.querySelector("#Registro-password").value;
-    const Registroemail = document.querySelector("#Registro-email").value;
-    auth
-    .createUserWithEmailAndPassword(Registroemail, Registropassword)
-    .then(userCredential => {
-    Registroform.reset();
-    $('#RegistroModal').modal('hide');
-    })
-})
-
-///// ACCESO USUARIO /////
-const Accesoform = document.querySelector("#Acceso-form");
-Accesoform.addEventListener("submit", e => {
-    e.preventDefault();  
-    const Accesopassword = document.querySelector("#Acceso-password").value;
-    const Accesoemail = document.querySelector("#Acceso-email").value;
-    
-    auth
-    .signInWithEmailAndPassword(Accesoemail, Accesopassword)
-    .then(userCredential => {
-    Accesoform.reset();
-    $('#AccesoModal').modal('hide');
-    window.location.href = "https://agustinifd.github.io/Repositorio/home.html";
-    })
-    .catch(function(error) {
-        alert("ATENCION EMAIL O PASSWORD NO VALIDOS")
-    });
-    
-})
-
-//////////SALIR/////////////////
-const Salir = document.querySelector("#Salir");
-Salir.addEventListener("click", e => {
-    e.preventDefault();  
-    auth.signOut().then(() => {
-        alert("Has salido con exito")
-        window.location.href = "https://agustinifd.github.io/Repositorio/index.html"
-        })
-})
-
-////////  ESTADO DE AUTH //////
-auth.onAuthStateChanged(user =>
-    {
-        if (user)
-        {
-            loginCheck(user)
-        }
-        else {
-            loginCheck(user) }
-        })
-    
-
-///////// GOOGLE LOGIN ///////
-const goglebtn = document.querySelector("#gogle");
-goglebtn.addEventListener("click", e => {
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider)
-    .then(result => {
-        Accesoform.reset();
-        $('#AccesoModal').modal('hide');
-        window.location.href = "https://agustinifd.github.io/Repositorio/home.html";
-    })
-})
-
 const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
 const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
 const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
@@ -89,7 +6,6 @@ const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
-
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -123,11 +39,6 @@ var getJSONData = function(url){
         return result;
     });
 }
-   
-        
-  
-
-
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
--- js/login.js
+++ js/login.js
@@ -1,22 +1,82 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const sinlogear = document.querySelectorAll(".sinlogear")
+const logeado = document.querySelectorAll(".logeado")
 
-});
-
-function validar()
-{
-    var validarusr = document.getElementById("Usuario").value
-    var validarpasw = document.getElementById("clave").value
-    if (validarusr == "" || validarpasw == ""){
-    alert("Debes llenar ambos campos")
-    }
-    else
-    { 
-    return window.location.href = "home.html";
-}}
+const loginCheck = user => {
+    if (user) {
+logeado.forEach(link => link.style.display = "block");
+sinlogear.forEach(link => link.style.display = "none");
+} else {
+    logeado.forEach(link => link.style.display = "none");
+    sinlogear.forEach(link => link.style.display = "block")
+}
+}
 
 
+//// REGISTRO USUARIO /////
 
+const Registroform = document.querySelector("#Registro-form");
+Registroform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const Registropassword = document.querySelector("#Registro-password").value;
+    const Registroemail = document.querySelector("#Registro-email").value;
+    auth
+    .createUserWithEmailAndPassword(Registroemail, Registropassword)
+    .then(userCredential => {
+    Registroform.reset();
+    $('#RegistroModal').modal('hide');
+    })
+})
 
+///// ACCESO USUARIO /////
+const Accesoform = document.querySelector("#Acceso-form");
+Accesoform.addEventListener("submit", e => {
+    e.preventDefault();  
+    const Accesopassword = document.querySelector("#Acceso-password").value;
+    const Accesoemail = document.querySelector("#Acceso-email").value;
+    
+    auth
+    .signInWithEmailAndPassword(Accesoemail, Accesopassword)
+    .then(userCredential => {
+    Accesoform.reset();
+    $('#AccesoModal').modal('hide');
+    window.location.href = "https://agustinifd.github.io/Repositorio/home.html";
+    })
+    .catch(function(error) {
+        alert("ATENCION EMAIL O PASSWORD NO VALIDOS")
+    });
+    
+})
+
+//////////SALIR/////////////////
+const Salir = document.querySelector("#Salir");
+Salir.addEventListener("click", e => {
+    e.preventDefault();  
+    auth.signOut().then(() => {
+        alert("Has salido con exito")
+        window.location.href = "https://agustinifd.github.io/Repositorio/index.html"
+        })
+})
+
+////////  ESTADO DE AUTH //////
+auth.onAuthStateChanged(user =>
+    {
+        if (user)
+        {
+            loginCheck(user)
+        }
+        else {
+            loginCheck(user) }
+        })
+    
+
+///////// GOOGLE LOGIN ///////
+const goglebtn = document.querySelector("#gogle");
+goglebtn.addEventListener("click", e => {
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider)
+    .then(result => {
+        Accesoform.reset();
+        $('#AccesoModal').modal('hide');
+        window.location.href = "https://agustinifd.github.io/Repositorio/home.html";
+    })
+})
