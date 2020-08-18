5cc1d4a5d7c4cd34c8990daaac30b6add251c8d4
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-08-18 17:20:32 -0300

    hfhg
    hgh

--- js/products.js
+++ js/products.js
@@ -5,8 +5,19 @@ document.addEventListener("DOMContentLoaded", function(e) {
 .then(data =>{
     let prueba = ""
     for (let i = 0; i < data.length; i++) {
-       prueba = prueba + "<div class=" + "containers" + i + ">Nombre: " + data[i].name + "<br>" + "Descripcion: " + data[i].description + "<br>" + "Precio: " + data[i].cost + data[i].currency + "<br>" + "Vendidos: " + data[i].soldCount + "</div>" + "<img src=" + data[i].imgSrc + ">"
-    }
+       prueba = prueba + `<div class="row">
+       <div class="col-3">
+           <img src="` + data[i].imgSrc + `" class="img-thumbnail">
+       </div>
+       <div class="col">
+           <div class="d-flex w-100 justify-content-between">
+               <h4 class="mb-1">` + data[i].name + `</h4>
+               <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
+           </div>
+           <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
+       </div>
+   </div>`
+   }
     document.getElementById("prueba").innerHTML = prueba;
     })
 });
