93473330520ee5b38659a4022ad86f9ce6ce8ebb
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-08-25 13:17:44 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -104,3 +104,33 @@ for (let i = 0; i < data.length; i++) {
 
 document.getElementById("cat-list-container").innerHTML = listafinalproductos
 })};
+
+function filtroproducto()
+{   
+var filtro = 0
+document.getElementById(filtroproducto.value) = filtro
+    fetch(PRODUCTS_URL)
+    .then(data => data.json())
+    .then(data =>{
+    listafinalproductos = "";
+for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if (parseInt(data[i].cost) > filtro) {
+        
+    
+    listafinalproductos += "<li>" + `<div class="row">
+    <div class="col-3">
+        <img src="` + data[i].imgSrc + `" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">` + data[i].name + `</h4>
+            <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
+        </div>
+        <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
+    </div>
+</div>` + "</li>"};
+
+document.getElementById("cat-list-container").innerHTML = listafinalproductos
+}
+})};
