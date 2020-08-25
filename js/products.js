641e28214f058bc7d18425a649e2e6c747185683
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-08-25 14:18:55 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -90,18 +90,7 @@ listafinalproductos = "";
 for (let i = 0; i < data.length; i++) {
     const element = data[i];
     if (parseInt(data[i].cost) < txtfiltro.value) {
-        listafinalproductos += "<li>IFFF" + `<div class="row">
-        <div class="col-3">
-            <img src="` + data[i].imgSrc + `" class="img-thumbnail">
-        </div>
-        <div class="col">
-            <div class="d-flex w-100 justify-content-between">
-                <h4 class="mb-1">` + data[i].name + `</h4>
-                <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
-            </div>
-            <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
-        </div>
-    </div>` + "</li>";}
+        listafinalproductos += "";}
     else {
     listafinalproductos += "<li>ELSE" + `<div class="row">
     <div class="col-3">
