f745bdbd7d43e5c44e879228a4ee4c2696c0fcf0
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-08-25 12:01:10 -0300

    gfh
    fgh

--- js/products.js
+++ js/products.js
@@ -3,21 +3,77 @@ document.addEventListener("DOMContentLoaded", function(e) {
     fetch(PRODUCTS_URL)
 .then(data => data.json())
 .then(data =>{
-    let prueba = ""
+    let listafinalproductos = ""
+    data.sort(function(a, b)
+    {return b.name - a.name;});
+    listafinalproductos = "";
     for (let i = 0; i < data.length; i++) {
-       prueba = prueba + `<div class="row">
-       <div class="col-3">
-           <img src="` + data[i].imgSrc + `" class="img-thumbnail">
-       </div>
-       <div class="col">
-           <div class="d-flex w-100 justify-content-between">
-               <h4 class="mb-1">` + data[i].name + `</h4>
-               <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
-           </div>
-           <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
-       </div>
-   </div>`
-   }
-    document.getElementById("prueba").innerHTML = prueba;
-    })
+    const element = data[i];
+    listafinalproductos += "<li>" + `<div class="row">
+    <div class="col-3">
+        <img src="` + data[i].imgSrc + `" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">` + data[i].name + `</h4>
+            <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
+        </div>
+        <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
+    </div>
+</div>` + "</li>"};
+    document.getElementById("cat-list-container").innerHTML = listafinalproductos;
+    
 });
+});
+////////////////// ORDEN DESENDIENTE ////////////////////////
+    function ordenardsc()
+{   
+    fetch(PRODUCTS_URL)
+    .then(data => data.json())
+    .then(data =>{
+    data.sort(function(a, b)
+    {return b.name > a.name;});
+    listafinalproductos = "";
+for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    listafinalproductos += "<li>" + `<div class="row">
+    <div class="col-3">
+        <img src="` + data[i].imgSrc + `" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">` + data[i].name + `</h4>
+            <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
+        </div>
+        <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
+    </div>
+</div>` + "</li>"};
+
+document.getElementById("cat-list-container").innerHTML = listafinalproductos
+})};
+//////////////////////// ORDEN ASENDIENDTE /////////////////
+function ordenarasc()
+{   
+    fetch(PRODUCTS_URL)
+    .then(data => data.json())
+    .then(data =>{
+    data.sort(function(a, b)
+    {return a.name > b.name;});
+    listafinalproductos = "";
+for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    listafinalproductos += "<li>" + `<div class="row">
+    <div class="col-3">
+        <img src="` + data[i].imgSrc + `" class="img-thumbnail">
+    </div>
+    <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">` + data[i].name + `</h4>
+            <small class="text-muted">` + "Precio: " + data[i].cost + data[i].currency + `</small>
+        </div>
+        <p class="mb-1">` + data[i].description + "<br>Unidades Vendidas:" + data[i].soldCount + `</p>
+    </div>
+</div>` + "</li>"};
+
+document.getElementById("cat-list-container").innerHTML = listafinalproductos
+})};
