cc96783359f48a748b91a666c9182e31821e7780
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-01 15:51:15 -0300

    gg
    gfg

--- js/product-info.js
+++ js/product-info.js
@@ -3,12 +3,19 @@
 //elementos HTML presentes.
 
 document.addEventListener("DOMContentLoaded", function(e){
-fetch("https://japdevdep.github.io/ecommerce-api/product/5678.json")
-.then(function(response)
-{return response.json();
-})
-.then(function(productInfo)
-{
-document.getElementById("producto1nombre").innerHTML = productInfo.name
-})
-});
+    fetch("https://japdevdep.github.io/ecommerce-api/product/5678.json")
+    .then(function(response)
+    {return response.json();
+    })
+    .then(function(productInfo)
+    {
+        document.getElementById("divcont").innerHTML = '<ul><p class="p1">' + productInfo.category + 
+        '</p><li id="producto1"><img src=' + productInfo.images[0] + '><p class="p2">' + productInfo.name +
+        '<br>' + productInfo.cost + productInfo.currency + '</p><p class="p3"> Cantidad de Vendidos: ' + productInfo.soldCount + '</p></li><li id="producto1desc"><p class="p">Descripcion</p>'
+        + productInfo.description + '</li><table><td><a href="' + productInfo.images[1] + '" target="_blank"><img src="' + productInfo.images[1] + '"></a></td>' + 
+        '<td><a href="' + productInfo.images[0] + '" target="_blank"><img src=' + productInfo.images[0] + '></a></td>' + 
+        '<td><a href="' + productInfo.images[2] + '" target="_blank"><img src=' + productInfo.images[2] + '></a></td>' + 
+        '<td><a href="' + productInfo.images[3] + '" target="_blank"><img src=' + productInfo.images[3] + '></a></td>' + 
+        '<td><a href="' + productInfo.images[4] + '" target="_blank"><img src=' + productInfo.images[4] + '></a></td></table></ul></div></div>' 
+    });
+    });
