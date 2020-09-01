45adb703b3d13753e66223eb75c3785737541895
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-01 13:24:26 -0300

    fdssfd
    sdf

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+fetch("https://japdevdep.github.io/ecommerce-api/product/5678.json")
+.then(function(response)
+{return response.json();
+})
+.then(function(MyJson)
+{
+    console.log(MyJson);
+})
 });
