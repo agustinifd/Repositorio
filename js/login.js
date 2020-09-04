35288db5c0f4abf1a3e40d060fe9a72213225c65
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-04 16:57:16 -0300

    RESUBIDO TODO
    TODO RESUBIDO

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 /////// LOGIN CHECK ////////
+
 const sinlogear = document.querySelectorAll(".sinlogear")
 const logeado = document.querySelectorAll(".logeado")
+
+
 
 let loginCheck = user => {
     if (user) {
