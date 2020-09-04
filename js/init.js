13499cfd5ffdfc296505d5e0f603442a5e598e31
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-04 17:33:13 -0300

    fhfh
    gffhffh

--- js/init.js
+++ js/init.js
@@ -7,7 +7,33 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAIcCtwoX_ch7-d6ibphLJcyhiKgkDwSWk",
+    authDomain: "prueba-oauth-286403.firebaseapp.com",
+    databaseURL: "https://prueba-oauth-286403.firebaseio.com",
+    projectId: "prueba-oauth-286403",
+    storageBucket: "prueba-oauth-286403.appspot.com",
+    messagingSenderId: "829078339573",
+    appId: "1:829078339573:web:c23f82ba4ba965ab7eaabc",
+    measurementId: "G-FT20SWH0YN"
+  };
 
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const auth = firebase.auth();
+  const fs = firebase.firestore();
+  let asd = ""
+
+ var remplazo = document.getElementById("profile")
+    function remplazito(){
+      if (sessionStorage.length > 0){
+      remplazo.innerText = sessionStorage.getItem("nombre")}
+      else{ remplazo.innerText = "Anonimo"}
+    };
+    remplazito();
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
