fc2d80dbb64d96fd72e4ad9e3b33a6e27747eedf
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-04 17:53:56 -0300

    fsd
    fsd

--- js/product-info.js
+++ js/product-info.js
@@ -20,5 +20,20 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
     fetch(PRODUCT_INFO_COMMENTS_URL)
     .then(function(Response)
-    {console.log("putput");})
+    {let comentarios = Response.json();
+    for (let i = 0; i < comentarios.length; i++) {
+    document.getElementById("comentarios").innerHTML = document.getElementById("comentarios").innerHTML + `<div class="card">
+    <div class="card-header">
+      ${comentarios.user}
+    </div>
+    <div class="card-body">
+      <blockquote class="blockquote mb-0">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+      </blockquote>
+    </div>
+  </div>`
+        
+    }
+    })
     });
