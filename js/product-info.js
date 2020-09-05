b5efeeb9ba9c4a985afc7cb36be36567debde042
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-05 17:38:15 -0300

    sd
    ds

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,7 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-btncalificar.document.addEventListener("click",comentar)
+document.getElementById("btncalificar").addEventListener("click",comentar)
 document.addEventListener("DOMContentLoaded", function(e){
     fetch("https://japdevdep.github.io/ecommerce-api/product/5678.json")
     .then(function(response)
