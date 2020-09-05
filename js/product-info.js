64ed05bdb47424d05122b95eb92bfd80c36ca331
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-05 17:49:29 -0300

    das
    das

--- js/product-info.js
+++ js/product-info.js
@@ -65,7 +65,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     }})});
 function comentar()
 {
-  console.log("entrofuncion")
+  var time = new Date();
+
   let estrellita = `<span class="fa fa-star checked"></span>`
   document.getElementById("comentarios").innerHTML = document.getElementById("comentarios").innerHTML + `<div class="card" style="width: 18rem;">
   <div class="card-header">
@@ -74,7 +75,7 @@ function comentar()
   <div class="card-body">
     <blockquote class="blockquote mb-0">
       <p>${document.getElementById("comentarionuevo").value}</p>
-      <footer class="blockquote-footer">${document.getElementById("profile")}<cite title="Source Title"><br>Publicado:</cite></footer>
+      <footer class="blockquote-footer">${document.getElementById("profile").value}<cite title="Source Title"><br>Publicado:${ time.toLocaleString('es-ES', { hour: 'numeric', hour12: true })}</cite></footer>
     </blockquote>
   </div>
 </div>`;
