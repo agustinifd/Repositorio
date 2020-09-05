cb85f6e8575e5acd6ba1f4a8be4cb98a3e088ccd
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-05 19:03:54 -0300

    fghghf
    hfgh

--- js/product-info.js
+++ js/product-info.js
@@ -74,7 +74,7 @@ function comentar()
   <div class="card-body">
     <blockquote class="blockquote mb-0">
       <p>${document.getElementById("comentarionuevo").value}</p>
-      <footer class="blockquote-footer">${document.getElementById("profile").textContent}<cite title="Source Title"><br>Publicado:${time.getFullYear() + `-` + time.getMonth() + `-`  + time.getDay() + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()}</cite></footer>
+      <footer class="blockquote-footer">${document.getElementById("profile").textContent}<cite title="Source Title"><br>Publicado:${time.getFullYear() + `-` + time.getMonth()+1 + `-`  + time.getDay()+1 + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()}</cite></footer>
     </blockquote>
   </div>
 </div>`;
