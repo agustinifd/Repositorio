3350543ccc17730b0f0b2499cc46e154612d74e5
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-11 10:54:51 -0300

    hf
    f

--- js/product-info.js
+++ js/product-info.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function(e){
           <p class="card-text">Descripción:${productInfo.description}</p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Cantidad de Vendidos: '${productInfo.soldCount}</li>
+          <li class="list-group-item">Cantidad de Vendidos: ${productInfo.soldCount}</li>
           <li class="list-group-item">A solo ${productInfo.cost}${productInfo.currency}</li>
           <li class="list-group-item">${productInfo.relatedProducts}</li>
         </ul>
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function(e){
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
-      </div>`;
+      </div><hr>`;
       for (let i= 0; i < todos.length; i++) {
          ////BUSCA DE 0 HASTA LA CANTIDAD DE ELEMENTOS QUE HAY EN TODOS LOS PRODUCTOS
         const element = todos[i];
@@ -67,7 +67,8 @@ document.addEventListener("DOMContentLoaded", function(e){
         {
         ////ENTRA A LA LISTA DE TODOS LOS AUTOS 
          
-      document.getElementById("divcont").innerHTML += `<div class="card related" style="width: 15rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top" > <div class="card-body"> <h5 class="card-title">${todos[productInfo.relatedProducts[i]].name}</h5>  <p class="card-text">Descripción:${todos[productInfo.relatedProducts[i]].description}</p></div><ul class="list-group list-group-flush"> <li class="list-group-item">Cantidad de Vendidos: '${todos[productInfo.relatedProducts[i]].soldCount}</li>  <li class="list-group-item">A solo ${todos[productInfo.relatedProducts[i]].cost}${todos[productInfo.relatedProducts[i]].currency}</li></ul> <div class="card-body"><a href="#" class="card-link">Conocelo Aqui</a> </div> </div>`;
+      document.getElementById("divcont").innerHTML += `<div class="card related" style="width: 18rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top">
+      <div class="card-body"> <h5 class="card-title">${todos[productInfo.relatedProducts[i]].name}</h5>  <p class="card-text">Descripción:${todos[productInfo.relatedProducts[i]].description}</p></div><ul class="list-group list-group-flush"> <li class="list-group-item">Cantidad de Vendidos: ${todos[productInfo.relatedProducts[i]].soldCount}</li>  <li class="list-group-item">A solo: ${todos[productInfo.relatedProducts[i]].cost}${todos[productInfo.relatedProducts[i]].currency}</li></ul> <div class="card-body"><a href="#" class="card-link">Conocelo Aqui</a> </div> </div><hr>`;
     }}
     })});
     fetch(PRODUCT_INFO_COMMENTS_URL)
