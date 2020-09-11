b75bb4cba4b7981b7196baf12843c60beefc8326
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-11 11:01:01 -0300

    fhgfhgf
    fdh

--- js/product-info.js
+++ js/product-info.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         {
         ////ENTRA A LA LISTA DE TODOS LOS AUTOS 
          
-      document.getElementById("divcont").innerHTML += `<div class="card related" style="width: 18rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top">
+      document.getElementById("divcont").innerHTML += `<div class="card related" style="width: 20rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top">
       <div class="card-body"> <h5 class="card-title">${todos[productInfo.relatedProducts[i]].name}</h5>  <p class="card-text">Descripción:${todos[productInfo.relatedProducts[i]].description}</p></div><ul class="list-group list-group-flush"> <li class="list-group-item">Cantidad de Vendidos: ${todos[productInfo.relatedProducts[i]].soldCount}</li>  <li class="list-group-item">A solo: ${todos[productInfo.relatedProducts[i]].cost}${todos[productInfo.relatedProducts[i]].currency}</li></ul> <div class="card-body"><a href="#" class="card-link">Conocelo Aqui</a> </div> </div>`;
     }}
     })});
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function(e){
       let estrellita = `<span class="fa fa-star checked"></span>`
     for (let i = 0; i < comentarios.length; i++) {
         const element = comentarios[i];
-        document.getElementById("comentarios").innerHTML =  document.getElementById("comentarios").innerHTML + `<div class="card grid-item" style="width: 20rem;">
+        document.getElementById("comentarios").innerHTML =  document.getElementById("comentarios").innerHTML + `<div class="card grid-item" style="width: 18rem;">
         <div class="card-header">
         ${estrellita.repeat(comentarios[i].score)}
         </div>
