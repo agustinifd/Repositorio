838bf6828ccb78c2b200c3a8b3c775050235f68b
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-11 10:42:30 -0300

    bcvvbc
    bvcbcvbcv

--- js/product-info.js
+++ js/product-info.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         {
         ////ENTRA A LA LISTA DE TODOS LOS AUTOS 
          
-      document.getElementById("divcont").innerHTML += `<div class="card" style="width: 15rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top" > <div class="card-body"> <h5 class="card-title">${todos[productInfo.relatedProducts[i]].name}</h5>  <p class="card-text">Descripción:${todos[productInfo.relatedProducts[i]].description}</p></div><ul class="list-group list-group-flush"> <li class="list-group-item">Cantidad de Vendidos: '${todos[productInfo.relatedProducts[i]].soldCount}</li>  <li class="list-group-item">A solo ${todos[productInfo.relatedProducts[i]].cost}${todos[productInfo.relatedProducts[i]].currency}</li></ul> <div class="card-body"><a href="#" class="card-link">Conocelo Aqui</a> </div> </div>`;
+      document.getElementById("divcont").innerHTML += `<div display: "inline-block" class="card" style="width: 15rem;"> <img src=${todos[productInfo.relatedProducts[i]].imgSrc} class="card-img-top" > <div class="card-body"> <h5 class="card-title">${todos[productInfo.relatedProducts[i]].name}</h5>  <p class="card-text">Descripción:${todos[productInfo.relatedProducts[i]].description}</p></div><ul class="list-group list-group-flush"> <li class="list-group-item">Cantidad de Vendidos: '${todos[productInfo.relatedProducts[i]].soldCount}</li>  <li class="list-group-item">A solo ${todos[productInfo.relatedProducts[i]].cost}${todos[productInfo.relatedProducts[i]].currency}</li></ul> <div class="card-body"><a href="#" class="card-link">Conocelo Aqui</a> </div> </div>`;
     }}
     })});
     fetch(PRODUCT_INFO_COMMENTS_URL)
