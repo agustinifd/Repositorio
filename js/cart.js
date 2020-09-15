42fac17c60a37de900f456faddd22d1558bc5346
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-15 10:10:00 -0300

    fdd
    gffg

--- js/cart.js
+++ js/cart.js
@@ -1,23 +1,19 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-let articulos = [];
-
 document.addEventListener("DOMContentLoaded", function(e){
     fetch("https://japdevdep.github.io/ecommerce-api/cart/987.json")
     .then(function(response) {
         return response.json();
     })
       .then(function(articulo) {
-        articulos = articulo;
-        console.log(articulo.articles[0].name)
     document.querySelector("#MostrarCart").innerHTML = `<ul class="list-unstyled">
     <li class="media">
-      <img src=${articulos.articles[0].src} class="mr-3" alt="...">
+      <img src=${articulo.articles[0].src} class="mr-3" alt="...">
       <div class="media-body">
-         <h5 class="mt-0 mb-1">${articulos.articles[0].name}</h5>
-         Cantidad: ${articulos.articles[0].count} costo unitario ${articulos.articles[0].unitCost}${articulos.articles[0].currency}
-        <br> Costo total: ${(articulos.articles[0].count)*(articulos.articles[0].unitCost)}${articulos.articles[0].currency}
+         <h5 class="mt-0 mb-1">${articulo.articles[0].name}</h5>
+         Cantidad: ${articulo.articles[0].count} costo unitario ${articulo.articles[0].unitCost}${articulo.articles[0].currency}
+        <br> Costo total: ${(articulo.articles[0].count)*(articulo.articles[0].unitCost)}${articulo.articles[0].currency}
        </div>
      </li>`;
         
