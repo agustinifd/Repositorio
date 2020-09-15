bd82d314dede13f0fa756391501276c99e144bfd
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-15 09:54:52 -0300

    hfd
    fg

--- js/cart.js
+++ js/cart.js
@@ -2,17 +2,26 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 let articulos = [];
+function mostrarCart()
+{
+    document.querySelector("#MostrarCart").innerHTML = `<ul class="list-unstyled">
+    <li class="media">
+      <img src=${articulos.articles[0].src} class="mr-3" alt="...">
+      <div class="media-body">
+        <h5 class="mt-0 mb-1">${articulos.articles[0].name}</h5>
+        Cantidad: ${articulos.articles[0].count} costo unitario ${articulos.articles[0].unitCost}${articulos.articles[0].currency}
+        <br> Costo total: ${(articulos.articles[0].count)*(articulos.articles[0].unitCost)}${articulos.articles[0].currency}
+      </div>
+    </li>`;
+}
 console.log(articulos)
 document.addEventListener("DOMContentLoaded", function(e){
 fetch("https://japdevdep.github.io/ecommerce-api/cart/987.json")
 .then(function(response) {
     return response.json();
-  })
+})
   .then(function(articulo) {
     articulos = articulo;
-    console.log(articulos.articles)
-    console.log(articulos.articles[0].name)
-    console.log(articulos.articles.name)
-  });
 });
-
+mostrarCart()
+});
