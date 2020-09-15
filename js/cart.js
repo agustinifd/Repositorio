38efa6702511d031851e8404bf0dc223ed1291b6
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-15 09:26:45 -0300

    bcvbvc
    bcvvbc

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+let articulos = [];
+console.log(articulos)
 document.addEventListener("DOMContentLoaded", function(e){
-
+fetch("https://japdevdep.github.io/ecommerce-api/cart/987.json")
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(articulo) {
+    articulos = articulo;
+    console.log(articulos)
+  });
 });
+
