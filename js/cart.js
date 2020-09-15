035ac93b1202a93fb64b30fe3df5f4ae9c1f9da1
agustinifd <68905493+agustinifd@users.noreply.github.com>
2020-09-15 11:50:48 -0300

    hfgh
    hfg

--- js/cart.js
+++ js/cart.js
@@ -19,12 +19,12 @@ document.addEventListener("DOMContentLoaded", function(e){
         <div class="card wish-list mb-3">
           <div class="card-body">
   
-            <h5 class="mb-4">Carrito (<span>${articulo.length}</span> items)</h5>
+            <h5 class="mb-4">Carrito (<span>${articulo.articles.length}</span> items)</h5>
               <div class="row mb-4">
               <div class="col-md-5 col-lg-3 col-xl-3">
                 <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
                   <img class="img-fluid w-100"
-                    src=${articulo.src} alt="Sample">
+                    src=${articulo.articles[0].src} alt="Sample">
                   <a href="#!">
                     <div class="mask waves-effect waves-light">
                       <div class="mask rgba-black-slight waves-effect waves-light"></div>
@@ -36,14 +36,14 @@ document.addEventListener("DOMContentLoaded", function(e){
                 <div>
                   <div class="d-flex justify-content-between">
                     <div>
-                      <h5>${articulo.name}</h5>
+                      <h5>${articulo.articles[0].name}</h5>
                       <p class="mb-3 text-muted text-uppercase small">Delicioso olor a monte nativo de pinos</p>
                     </div>
                     <div>
                       <div class="def-number-input number-input safari_only mb-0 w-100">
                         <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
                           class="minus"></button>
-                        <input class="quantity" min="0" name="quantity" value=${articulo.count} type="number">
+                        <input class="quantity" min="0" name="quantity" value=${articulo.articles[0].count} type="number">
                         <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
                           class="plus"></button>
                       </div>
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                       <a href="#!" type="button" class="card-link-secondary small text-uppercase"><i
                           class="fas fa-heart mr-1"></i> Move to wish list </a>
                     </div>
-                    <p class="mb-0"><span><strong>${articulo.unitCost}${articulo.currency}</strong></span></p>
+                    <p class="mb-0"><span><strong>${articulo.articles[0].unitCost}${articulo.articles[0].currency}</strong></span></p>
                   </div>
                 </div>
               </div>
@@ -113,7 +113,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             <ul class="list-group list-group-flush">
               <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
                 Temporary amount
-                <span>${(articulo.cost)*(articulo.count)}</span>
+                <span>${(articulo.articles[0].cost)*(articulo.articles[0].count)}</span>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center px-0">
                 Shipping
@@ -126,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                     <p class="mb-0">(incluido envio)</p>
                   </strong>
                 </div>
-                <span><strong>${(articulo.cost)*(articulo.count)}</strong></span>
+                <span><strong>${(articulo[0].cost)*(articulo[0].count)}</strong></span>
               </li>
             </ul>
               <button type="button" class="btn btn-primary btn-block waves-effect waves-light">go to checkout</button>
